7a80cfb92dc39f5b9696d573e8629d67a4e5be1e
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-08 21:05:34 +0530

    function

--- func.js
+++ func.js
@@ -1,56 +1,70 @@
-function intro(){
-    console.log(" hello everyone !");
+// function intro(){
+//     console.log(" hello everyone !");
+// }
+// intro();
+
+// function intro2(username , age ){
+//     console.log(`hello my name is ${username}`);
+//     console.log(`and my age is ${age}`)
+// }
+
+// intro2("sush",21);
+//  intro2(" harshitha",16);
+
+
+// // calculator 
+
+// function add(x, y){
+//     return x+y;
+// }
+// console.log(add(5,2));
+
+// function sub(x, y){
+//     return x-y;
+// }
+// console.log(sub(5,2));
+
+// function mul(x, y){
+//     return x*y;
+// }
+// console.log(mul(5,2));
+
+// function div(x, y){
+//     return x/y;
+// }
+// console.log(div(5,2));
+
+
+// /// function to check whether it is even or odd 
+
+// function isEven(num){
+//     return num%2===0 ? true :false;
+// }
+
+// console.log(isEven(4));
+
+// // function to checkmwhether the given email is valid or not 
+
+// function isValidEmail(email){
+//     if (email.includes("@" && ".com")){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// console.log(isValidEmail(" sushgamil"));
+
+// // function as expression 
+
+var result= function sum(a, b){
+    return a+b;
 }
-intro();
+console.log(result(5,10));
 
-function intro2(username , age ){
-    console.log(`hello my name is ${username}`);
-    console.log(`and my age is ${age}`)
+// Anonymous funnction 
+
+var result = function(a,b){
+    return a+b;
 }
-
-intro2("sush",21);
- intro2(" harshitha",16);
-
-
-// calculator 
-
-function add(x, y){
-    return x+y;
-}
-console.log(add(5,2));
-
-function sub(x, y){
-    return x-y;
-}
-console.log(sub(5,2));
-
-function mul(x, y){
-    return x*y;
-}
-console.log(mul(5,2));
-
-function div(x, y){
-    return x/y;
-}
-console.log(div(5,2));
-
-
-/// function to check whether it is even or odd 
-
-function isEven(num){
-    return num%2===0 ? true :false;
-}
-
-console.log(isEven(4));
-
-// function to checkmwhether the given email is valid or not 
-
-function isValidEmail(email){
-    if (email.includes("@" && ".com")){
-        return true;
-    }else{
-        return false;
-    }
-}
-console.log(isValidEmail(" sushgamil"));
+console.log(result(2,9));
 
