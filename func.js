48172ff5ce7b6d8b37afe551f27cc7cd81d74eea
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-08 19:54:43 +0530

    function

---
+++ func.js
@@ -0,0 +1,56 @@
+function intro(){
+    console.log(" hello everyone !");
+}
+intro();
+
+function intro2(username , age ){
+    console.log(`hello my name is ${username}`);
+    console.log(`and my age is ${age}`)
+}
+
+intro2("sush",21);
+ intro2(" harshitha",16);
+
+
+// calculator 
+
+function add(x, y){
+    return x+y;
+}
+console.log(add(5,2));
+
+function sub(x, y){
+    return x-y;
+}
+console.log(sub(5,2));
+
+function mul(x, y){
+    return x*y;
+}
+console.log(mul(5,2));
+
+function div(x, y){
+    return x/y;
+}
+console.log(div(5,2));
+
+
+/// function to check whether it is even or odd 
+
+function isEven(num){
+    return num%2===0 ? true :false;
+}
+
+console.log(isEven(4));
+
+// function to checkmwhether the given email is valid or not 
+
+function isValidEmail(email){
+    if (email.includes("@" && ".com")){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isValidEmail(" sushgamil"));
+
