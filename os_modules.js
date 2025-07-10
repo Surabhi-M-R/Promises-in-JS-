6f9fea05bf681f47662a517ffee84ffd0bcf002b
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-10 13:54:45 +0530

    os modules

---
+++ os_modules.js
@@ -0,0 +1,10 @@
+const os=require('os');
+
+console.log('platform: ',os.platform());
+console.log('arch: ',os.arch());
+console.log('total up time: ',os.totalmem());
+console.log('homedir: ',os.homedir());
+console.log('user info: ',os.userInfo());
+console.log('type: ',os.type());
+console.log('temp directory : ',os.tmpdir());
+console.log('home: ',os.hostname());
